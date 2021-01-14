a1ad84b8597117e27d9b2e5d66c43d0a75b9be9d
veroca88 <veronicadonoso1506@gmail.com>
2021-01-14 08:51:48 -0600

    Return to the first stage

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,25 +1,15 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose
-//   .connect(process.env.MONGODB_URI, {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(x =>
-//     console.log(
-//       `Connected to Mongo! Database name: "${x.connections[0].name}"`,
-//     ),
-//   )
-//   .catch(err => console.error('Error connecting to mongo', err));
-
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.MONGO_URI
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(x =>
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`,
+    ),
+  )
+  .catch(err => console.error('Error connecting to mongo', err));
 
